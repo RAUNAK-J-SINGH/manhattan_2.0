8a9d3a1fc81cdaefd3e145d587ae7fbe6cdac817
RAUNAK-J-SINGH <raunakk487@gmail.com>
2023-07-26 11:50:49 +0530

    ./

---
+++ tkinter.py
@@ -0,0 +1,13 @@
+import tkinter as tk
+from tkinter import *
+from tkinter import filedialog
+from tkinter.ttk import Combobox
+import pyttsx3
+import os
+
+root = Tk()
+root.title("TEXT TO SPEECH")
+root.geometry("900x450")
+root.resizable(False,False)
+
+root.mainloop()
